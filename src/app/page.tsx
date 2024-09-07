df4eba05e16276915f73c96cf588cb77dde41ae3
alanas404 <al.anas404040@gmail.com>
2024-09-08 01:13:05 +0600

    some component included

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Newspaper from "./components/newspaper/Newspaper";
 import Blog from "./components/blogs/Blog";
 import Customer from "./components/customers/Customer";
+import Tabs from "./components/tabs/Tabs";
 
 export default function Home() {
   return (
     <div>
       <Newspaper/>
+      <Tabs/>
       <Customer/>
       <Blog/>
+  
  
     </div>
   );
