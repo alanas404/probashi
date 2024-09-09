5369b399a0ab7bd18f2cdcc17c2e3ee328b87e94
alanas404 <al.anas404040@gmail.com>
2024-09-09 23:30:54 +0600

    banner section designed

---
+++ src/app/components/downloads/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className='md:px-[135px] bg-[#F8FAFC]'>
+            <div className="px-4 py-[17px] md:px-0 md:pt-16 md:pb-8 bg-[url('/download/download_hero.svg')] bg-no-repeat bg-[right_bottom_24px] md:bg-right-bottom bg-[length:61px_44px] md:bg-[length:151px_107px]">
+                <h2 className='barlow font-semibold text-[22px]  md:text-[32px] leading-[26.4px] md:leading-10 text-[#0F172A] mb-2'>Download Certificate & Cards</h2>
+                <p className='barlow font-normal md:font-medium text-base md:text-xl leading-[16.8px] md:leading-6 text-[#64748B]'>BMET Registration, BMET Clearance, PDO Enrollment, PDO Certificate & Training Certificate  </p>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Newspaper from "./components/newspaper/Newspaper";
 import Blog from "./components/blogs/Blog";
 import Customer from "./components/customers/Customer";
 import Tabs from "./components/tabs/Tabs";
+import Hero from "./components/downloads/Hero";
 
 export default function Home() {
   return (
     <div>
+      <Hero/>
       {/* <Newspaper/> */}
       <Tabs/>
       {/* <Customer/>
